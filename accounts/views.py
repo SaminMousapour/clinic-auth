5216b797d8575882cd81607db09a573c1cf726c2
Clinic Admin <clinic@localhost>
2026-08-29 11:03:35 -0700

    Add settings import for test endpoints

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.hashers import make_password, check_password
 from django.utils import timezone
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
 from datetime import date, timedelta
 from .models import User, Doctor, Patient, Appointment, Medication, HealthReading, INSURANCE_CHOICES, PatientVisit, MedicalRecord, Prescription, PatientRecord
 import logging
